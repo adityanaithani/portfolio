e803c3548641385bd5a25de08ef603ace3ab777b
adityanaithani <aditya0602@gmail.com>
2024-07-14 17:32:49 -0400

    complete initial layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+    <a href="/projects">projects</a>
+	<a href="/blog">blog</a>
+</nav>
+
+<slot></slot>
+
+<footer>
+	<p>github/linkedin/email</p>
+</footer>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Layout from "./+layout.svelte";
+</script>
+<div class="flex-container">
+  <h1>hi i'm adi!</h1>
+</div>
