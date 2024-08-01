10244ae6eae4c7b80217f8b4f5aba6881d3c8024
adityanaithani <aditya0602@gmail.com>
2024-08-01 00:27:47 -0400

    install mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,23 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: [".svelte", ".md"],
+  preprocess: [
+    mdsvex({
+      // The default mdsvex extension is .svx; this overrides that.
+      extensions: [".md"],
+
+      // Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+      // rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    }),
+    vitePreprocess(),
+  ],
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
-  preprocess: vitePreprocess(),
 };
 
 export default config;
