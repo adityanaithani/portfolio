b6c44f6e5d3910a84825d24e36099a2bc5be4a30
adityanaithani <aditya0602@gmail.com>
2024-08-01 14:04:52 -0400

    config cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,6 @@ const config = {
     mdsvex({
       // The default mdsvex extension is .svx; this overrides that.
       extensions: [".md"],
-
-      // Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-      // rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
     }),
     vitePreprocess(),
   ],
