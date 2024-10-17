0c8d3e6243c0f25a76ebe35d14c0c255b2136ccb
adityanaithani <aditya0602@gmail.com>
2024-10-17 01:49:39 -0400

    feat: implement project page skeleton

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,4 +2,96 @@
   <title>adi's projects</title>
 </svelte:head>
 
-<h1>coming soon!</h1>
+<div>
+  <h2>projects</h2>
+  <div>
+    <details class="">
+      <summary class=""
+        ><a href="https://github.com/adityanaithani/coderacer"
+          ><u>coderacer</u>
+        </a></summary
+      >
+      <div class="text-sm">
+        <p>a programming language coderacer game</p>
+      </div>
+    </details>
+
+    <details>
+      <summary>
+        <a href="https://github.com/adityanaithani/themartian-ui">rover-im</a>
+      </summary>
+      <div>
+        <p>messaging ui from the martian (2015)</p>
+      </div>
+    </details>
+
+    <details>
+      <summary
+        ><a href="https://github.com/adityanaithani/genetools">genetools</a
+        ></summary
+      >
+      <div>
+        <p>svelte, express - process gene data</p>
+      </div>
+    </details>
+
+    <details>
+      <summary>
+        <a href="https://github.com/adityanaithani/jumbo-bot">jumbo</a>
+      </summary>
+      <div>
+        <p>python - flight tracker discord bot</p>
+      </div>
+    </details>
+
+    <details>
+      <summary>
+        <a href="https://github.com/umass-cs-326-s23/326-project-repo-team-33"
+          >driveU</a
+        >
+      </summary>
+      <div>
+        <p>express, mongodb - university ridesharing app</p>
+      </div>
+    </details>
+
+    <details>
+      <summary><a href="/projects">ai bias</a></summary>
+      <div>
+        <p>jupyter notebook - analyze bias in ai used in healthcare systems</p>
+      </div>
+    </details>
+
+    <details>
+      <summary>
+        <a href="/projects">bioinformatics research project</a>
+      </summary>
+      <div>
+        <p>pymol, BLAST - analyze bacteriophage structures</p>
+      </div>
+    </details>
+
+    <details>
+      <summary>
+        <a href="/projects">bioinfo paper</a>
+      </summary>
+      <div>
+        <p>
+          cowrote a paper on bacteriophage bioinformatics. currently awaiting
+          peer review.
+        </p>
+      </div>
+    </details>
+
+    <details>
+      <summary>
+        <a href="https://github.com/adityanaithani/TwitchGameTools"
+          >twitch game tools</a
+        >
+      </summary>
+      <div>
+        <p>dev kit for games that can interact with twitch chat</p>
+      </div>
+    </details>
+  </div>
+</div>
