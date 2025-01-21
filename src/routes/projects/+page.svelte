2ffbd1081f2ee45856937bbfe33a3d48739a80ac
adityanaithani <aditya0602@gmail.com>
2025-01-21 00:16:59 -0500

    feat:populate projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,94 +4,97 @@
 
 <div>
   <h2>projects</h2>
+
   <div>
-    <details class="">
-      <summary class=""
-        ><a href="https://github.com/adityanaithani/coderacer"
-          ><u>coderacer</u>
-        </a></summary
-      >
-      <div class="text-sm">
-        <p>a programming language coderacer game</p>
-      </div>
-    </details>
-
-    <details>
-      <summary>
-        <a href="https://github.com/adityanaithani/themartian-ui">rover-im</a>
-      </summary>
-      <div>
-        <p>messaging ui from the martian (2015)</p>
-      </div>
-    </details>
-
-    <details>
-      <summary
-        ><a href="https://github.com/adityanaithani/genetools">genetools</a
-        ></summary
-      >
-      <div>
-        <p>svelte, express - process gene data</p>
-      </div>
-    </details>
-
-    <details>
-      <summary>
-        <a href="https://github.com/adityanaithani/jumbo-bot">jumbo</a>
-      </summary>
-      <div>
-        <p>python - flight tracker discord bot</p>
-      </div>
-    </details>
-
-    <details>
-      <summary>
-        <a href="https://github.com/umass-cs-326-s23/326-project-repo-team-33"
-          >driveU</a
+    <ul>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="https://github.com/adityanaithani/coderacer">coderacer</a
+          ></span
         >
-      </summary>
-      <div>
-        <p>express, mongodb - university ridesharing app</p>
-      </div>
-    </details>
-
-    <details>
-      <summary><a href="/projects">ai bias</a></summary>
-      <div>
-        <p>jupyter notebook - analyze bias in ai used in healthcare systems</p>
-      </div>
-    </details>
-
-    <details>
-      <summary>
-        <a href="/projects">bioinformatics research project</a>
-      </summary>
-      <div>
-        <p>pymol, BLAST - analyze bacteriophage structures</p>
-      </div>
-    </details>
-
-    <details>
-      <summary>
-        <a href="/projects">bioinfo paper</a>
-      </summary>
-      <div>
-        <p>
-          cowrote a paper on bacteriophage bioinformatics. currently awaiting
+        -
+        <span class="text-sm">typescript, nodeJS, reactJS</span>
+        <p class="my-0 text-lg">programming language typeracer game</p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="https://github.com/adityanaithani/themartian-ui">rover-im</a
+          ></span
+        >
+        -
+        <span class="text-sm">javascript</span>
+        <p class="my-0 text-lg">ui recreation from the martian</p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="https://github.com/adityanaithani/genetools">genetools</a
+          ></span
+        >
+        -
+        <span class="text-sm">svelte, express, docker, mongodb</span>
+        <p class="my-0 text-lg">tool to process genetics data</p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="https://github.com/adityanaithani/jumbo-bot">jumbo</a></span
+        >
+        -
+        <span class="text-sm">python, discord.py</span>
+        <p class="my-0 text-lg">flight tracking discord bot</p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a
+            href="https://github.com/umass-cs-326-s23/326-project-repo-team-33"
+            >driveU</a
+          ></span
+        >
+        -
+        <span class="text-sm">express, mongodb</span>
+        <p class="my-0 text-lg">ridesharing app for college students</p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="/projects">ai bias</a></span
+        >
+        -
+        <span class="text-sm">jupyter notebook, python</span>
+        <p class="my-0 text-lg">
+          evaluating bias in medical image classification ml models
+        </p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="/projects">bacteriophage discovery</a></span
+        >
+        -
+        <span class="text-sm">python, pymol</span>
+        <p class="my-0 text-lg">
+          isolated, extracted, sequenced, annotated, then modelled bacteriophage
+          protein structures
+        </p>
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="/projects">bioinformatics research paper</a></span
+        >
+        -
+        <span class="text-sm">python</span>
+        <p class="my-0 text-lg">
+          cowrote a paper on bacteriophage bioinformatics - currently awaiting
           peer review.
         </p>
-      </div>
-    </details>
-
-    <details>
-      <summary>
-        <a href="https://github.com/adityanaithani/TwitchGameTools"
-          >twitch game tools</a
+      </li>
+      <li>
+        <span class="text-xl font-semibold underline"
+          ><a href="/projects">twitch game tools</a></span
         >
-      </summary>
-      <div>
-        <p>dev kit for games that can interact with twitch chat</p>
-      </div>
-    </details>
+        -
+        <span class="text-sm">nodeJS</span>
+        <p class="my-0 text-lg">
+          dev kit for games that can interact with twitch chat
+        </p>
+      </li>
+    </ul>
   </div>
 </div>
