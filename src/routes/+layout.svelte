dd064a98f3e695348e189593b32ddbcb40412100
adityanaithani <aditya0602@gmail.com>
2025-02-03 20:40:37 -0500

    fix:eliminate funky dark mode selector

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,5 @@
 <script>
   import "../app.css";
-  import { onMount } from "svelte";
-  import { fade } from "svelte/transition";
-
-  let theme = "neutral"; // neutral, light, dark
-
-  function toggleTheme() {
-    if (theme === "neutral") {
-      theme = "light";
-    } else if (theme === "light") {
-      theme = "dark";
-    } else {
-      theme = "neutral";
-    }
-    applyTheme();
-  }
-
-  function applyTheme() {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else if (theme === "light") {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    } else {
-      localStorage.removeItem("theme");
-    }
-  }
-
-  onMount(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      theme = savedTheme;
-      applyTheme();
-    }
-  });
-
-  $: hoverClass =
-    theme === "neutral"
-      ? "hover:text-copygrey"
-      : theme === "light"
-        ? "hover:text-mantis"
-        : "hover:text-pink";
 </script>
 
 <svelte:head>
@@ -59,13 +17,6 @@
       <a class="" href="/projects">projects</a>
       <a class="" href="/blog">blog</a>
       <a class="" href="/photos">photos</a>
-      <button
-        on:click={toggleTheme}
-        class="text-gray-800 dark:text-gray-20 {hoverClass}"
-      >
-        {theme === "neutral" ? "os" : theme === "light" ? "jedi" : "sith"}
-        <!-- listen i told you i liked star wars -->
-      </button>
     </nav>
 
     <slot></slot>
@@ -73,6 +24,12 @@
 
   <div class="pl-[100px]">
     <p id="socials" class="space-x-2">
+      <a
+        href="mailto:aditya.naith@protonmail.com"
+        target="_blank"
+        class="fa-solid fa-envelope fa-xl"
+        aria-label="Email"
+      ></a>
       <a
         href="https://linkedin.com/in/aditya-naithani/"
         target="_blank"
@@ -84,12 +41,6 @@
         target="_blank"
         class="fa-brands fa-github fa-xl"
         aria-label="GitHub"
-      ></a>
-      <a
-        href="mailto:aditya.naith@protonmail.com"
-        target="_blank"
-        class="fa-solid fa-envelope fa-xl"
-        aria-label="Email"
       ></a>
     </p>
   </div>
